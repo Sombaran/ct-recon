6ae91f909a1f074cb1252baabcb6573336ec4352
Sombaran Gupta <40040729+Sombaran@users.noreply.github.com>
2025-08-29 22:56:10 +0530

    253405A: Initial header version

--- include/testInc.h
+++ include/testInc.h
@@ -1,11 +1,20 @@
 #ifndef MY_CODE_H
 #define MY_CODE_H
 
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <syslog.h>
+#include <memory>
+
+
 class test
 {
     public:
     void foo();
-
+    void reverseNumber(int& );
 };
 #endif // MY_CODE_H
 
