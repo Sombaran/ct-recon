e6357a4dc5c8cbafff05e7272d03314f02afe8b2
Sombaran Gupta <guptasombaran@yahoo.com>
2025-08-29 22:42:01 +0530

    253405A:C++ initial version

--- source/main.cpp
+++ source/main.cpp
@@ -3,9 +3,14 @@
 #include "testInc.h"
 #include <iostream>
 
+int someNumber  {123};
+int someNumber1 {-123};
+
 int main()
 {
-    test t;
-    t.foo();
+    std::shared_ptr<test> lObj;
+    lObj->foo();
+    lObj->reverseNumber(someNumber);    // positive number
+    lObj->reverseNumber(someNumber1);   // negative number
     return (0);
 }
